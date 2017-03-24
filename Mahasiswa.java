421563abc77c4281f7ded9456b6e021c17eab3cc
fauzihzrn <fauzihzrn96@gmail.com>
2017-03-24 17:46:33 +0700

    Add files via upload

---
+++ Mahasiswa.java
@@ -0,0 +1,17 @@
+public class Mahasiswa {
+public String nama;
+
+public void kuliah () {
+System.out.println ("Kuliah");
+}
+
+public static void main (String [] args) {
+
+Mahasiswa andri = new Mahasiswa () ;
+
+andri.nama = "Andri Supiandi";
+andri.kuliah ();
+
+}
+
+}
